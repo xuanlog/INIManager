d0a4dcd0e46c9b790931468e66f533573fad4ed4
xuanlog <74643573+xuanlog@users.noreply.github.com>
2022-11-24 09:58:14 +0800

    初版提交

---
+++ INIManager.cpp
@@ -0,0 +1,119 @@
+#include "INIManager.h"
+#include <fstream>
+
+INIManager::INIManager(void)
+{
+    SetRoot("Config");
+    SetFilePath("Config.ini");
+    ReadFile();
+}
+
+INIManager::INIManager(const string &file_path, const string &root_name)
+{
+    SetRoot(root_name);
+    SetFilePath(file_path);
+    ReadFile();
+}
+
+INIManager::~INIManager(void)
+{
+    WriteFile();
+}
+
+string INIManager::GetValue(const string &key) const
+{
+    return GetValue(m_root, key);
+}
+
+string INIManager::GetValue(const string &root, const string &key) const
+{
+    if (key.empty()) {
+        return "";
+    }
+
+    auto itr = m_map.find(root);
+    if (itr != m_map.end()) {
+        auto s_itr = itr->second.find(key);
+        if (s_itr != itr->second.end()) {
+            return s_itr->second;
+        }
+    }
+
+    return "";
+}
+
+int INIManager::SetValue(const string &key, const string &val)
+{
+    return SetValue(m_root, key, val);
+}
+
+int INIManager::SetValue(const string &root, const string &key, const string &val)
+{
+    if (key.empty()) {
+        return -1;
+    }
+
+    auto itr = m_map.find(root);
+    if (m_map.end() == itr) {
+        SubNode tmp_node;
+        tmp_node.insert(pair<string, string>(key, val));
+        m_map.insert(pair<string, SubNode>(root, tmp_node));
+    } else {
+        itr->second[key] = val;
+    }
+
+    return 0;
+}
+
+void INIManager::WriteFile(void)
+{
+    ofstream fs;
+
+    fs.open(m_file_path, ios::out);
+    if (!fs.is_open()) {
+        return;
+    }
+
+    for (auto &itr : m_map) {
+        fs << "[" << itr.first << "]" << std::endl;
+        for (auto &s_itr : itr.second) {
+            fs << s_itr.first << " = " << s_itr.second << std::endl;
+        }
+    }
+
+    fs.close();
+}
+
+void INIManager::ReadFile(void)
+{
+    ifstream fs;
+
+    fs.open(m_file_path, ios::in);
+    if (!fs.is_open()) {
+        return;
+    }
+
+    string line;
+    string root;
+    string key;
+    string value;
+    int line_size = 0;
+    int equal_pos = 0;
+
+    while (getline(fs, line)) {
+        line_size = line.size();
+        equal_pos = line.find('=');
+
+        if ((0 == line.find('['))
+            && (line.find(']') == line_size - 1)) {
+            root = line.substr(1, line_size - 2);
+        } else if (equal_pos != string::npos) {
+            key = line.substr(0, equal_pos - 1);
+            value = line.substr(equal_pos + 2);
+        }
+
+        SetValue(root, key, value);
+    }
+
+    fs.close();
+}
---
+++ INIManager.h
@@ -0,0 +1,67 @@
+#ifndef _INI_MANAGER_H_
+#define _INI_MANAGER_H_
+
+#define USE_UNORDERED_MAP   1   /* 使用unordered_map */
+
+#if USE_UNORDERED_MAP
+#include <unordered_map>
+#else
+#include <map>
+#endif
+#include <string>
+
+#define CREATE_MEMBER_VAL_FUNC(Type, FuncName, MemberName, Permission) \
+Permission: \
+    Type m_##MemberName; \
+\
+public: \
+    void Set##FuncName(const Type &tmp_val) { \
+        m_##MemberName = tmp_val; \
+    } \
+\
+    Type Get##FuncName(void) const { \
+        return m_##MemberName; \
+    }
+
+using namespace std;
+
+#if USE_UNORDERED_MAP
+using SubNode = unordered_map<string, string>;
+#else
+using SubNode = map<string, string>;
+#endif
+
+class INIManager {
+public:
+    INIManager(void);
+
+    INIManager(const string &file_path, const string &root_name);
+
+    virtual ~INIManager(void);
+
+    string GetValue(const string &key) const;
+
+    string GetValue(const string &root, const string &key) const;
+
+    int SetValue(const string &key, const string &val);
+
+    int SetValue(const string &root, const string &key, const string &val);
+
+    int GetSize(void) const { return m_map.size(); }
+
+    CREATE_MEMBER_VAL_FUNC(string, Root, root, private)
+    CREATE_MEMBER_VAL_FUNC(string, FilePath, file_path, private)
+
+private:
+    void WriteFile(void);
+
+    void ReadFile(void);
+
+#if USE_UNORDERED_MAP
+    unordered_map<string, SubNode> m_map;
+#else
+    map<string, SubNode> m_map;
+#endif
+};
+
+#endif
